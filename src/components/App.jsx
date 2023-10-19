74940fe170eff6c47691180907009966667eaab7
Stee1Lemon <121914099+Stee1Lemon@users.noreply.github.com>
2023-10-19 11:31:13 +0100

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,6 +22,7 @@ export function App() {
   useEffect(() => {
     if (!query) return;
     setIsLoaded(true);
+    setError(false);
 
     async function fetchData() {
       try {
@@ -52,7 +53,6 @@ export function App() {
 
   const handlePictures = query => {
     setNoImagesFound(false);
-    setError(false);
     setQuery(query);
     setImagesToRender([]);
     setPage(1);
@@ -79,96 +79,3 @@ export function App() {
     </div>
   );
 }
-
-// export class App extends Component {
-//   state = {
-//     imagesToFind: '',
-//     currentPage: 1,
-//     imagesToRender: [],
-//     totalHits: 0,
-//     isLoaded: false,
-//     error: false,
-//     messageNotFound: false,
-//     modalOpen: false,
-//   };
-
-//   async componentDidUpdate(prevProps, prevState) {
-//     const { imagesToFind, currentPage } = this.state;
-
-//     if (
-//       prevState.imagesToFind !== imagesToFind ||
-//       prevState.currentPage !== currentPage
-//     ) {
-//       try {
-//         this.setState({ isLoaded: true, error: false, messageNotFound: false });
-//         const data = await fetchImages(imagesToFind, currentPage);
-//         if (!data.totalHits) {
-//           this.setState({
-//             messageNotFound: true,
-//           });
-//           return;
-//         }
-
-//         this.setState(prev => {
-//           return {
-//             imagesToRender: [...prev.imagesToRender, ...data.hits],
-//             totalHits: data.totalHits,
-//           };
-//         });
-//       } catch (error) {
-//         this.setState({ error: true });
-//       } finally {
-//         this.setState({ isLoaded: false });
-//       }
-//     }
-//   }
-
-//   openModal = imgUrl => {
-//     this.setState({ modalOpen: imgUrl });
-//   };
-
-//   closeModal = () => {
-//     this.setState({ modalOpen: false });
-//   };
-
-//   handlePictures = query => {
-//     this.setState({
-//       imagesToFind: query,
-//       imagesToRender: [],
-//       currentPage: 1,
-//       totalHits: 0,
-//     });
-//   };
-
-//   handleMorePictures = () => {
-//     this.setState(prevState => {
-//       return { currentPage: prevState.currentPage + 1 };
-//     });
-//   };
-
-//   render() {
-//     const {
-//       imagesToRender,
-//       isLoaded,
-//       error,
-//       messageNotFound,
-//       modalOpen,
-//       totalHits,
-//     } = this.state;
-//     return (
-//       <div className={styles.App}>
-//         <Searchbar onSubmit={this.handlePictures} />
-//         {isLoaded && <Loader />}
-//         {messageNotFound && <NotFoundMessage />}
-//         {error && <ErrorMassage />}
-//         {imagesToRender && (
-//           <ImageGallery images={imagesToRender} openModal={this.openModal} />
-//         )}
-//         {modalOpen && <Modal imgUrl={modalOpen} closeModal={this.closeModal} />}
-//         {!isLoaded && imagesToRender.length !== totalHits && (
-//           <ButtonMore morePictures={this.handleMorePictures} />
-//         )}
-//       </div>
-//     );
-//   }
-// }
